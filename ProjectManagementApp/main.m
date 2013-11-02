de0e8d757974ff5979a2ce92d42b63d43782004b
Georgi Ivanov <georgiivanov@Georgis-Mac.local>
2013-11-02 12:21:29 +0200

    Initial Commit

---
+++ ProjectManagementApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ProjectManagementApp
+//
+//  Created by Georgi Ivanov on 11/2/13.
+//  Copyright (c) 2013 Georgi Ivanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
